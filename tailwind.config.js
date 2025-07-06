a1dcc17617d8d3f345fc2e535790fb73aed921a3
okitomomo <t.okita.41119@gmail.com>
2025-07-06 21:46:07 +0900

    ヘッダー（仮）

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 export default {
     content: ['./**/*.html'],
-    theme: { extend: {} },
+    theme: { 
+      extend: {
+        fontFamily: {
+          def: ['Noto Sans JP', 'sans-serif'],
+          eng: ['Hanuman', 'serif'],
+          num: ['Cuprum', 'serif']
+        }
+      } 
+    },
     plugins: []
   }
   
